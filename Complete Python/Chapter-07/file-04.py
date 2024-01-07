103812190c4a4d2e9197b1a042014f409c72df20
Ayan <impracticalayan@gmail.com>
2024-01-08 01:28:11 +0530

    Strings Basic

---
+++ Complete Python/Chapter-07/file-04.py
@@ -0,0 +1,37 @@
+# METHODS
+
+# REPETITION OPERATOR (*)
+print("HELLO"*3)
+
+# CONCATENATION (+)
+print("HELLO"+" "+"WORLD!")
+
+# COMPARISON
+
+# Equality check
+str1 = "Hello"
+str2 = "hello"
+
+if str1 == str2:
+    print("The strings are equal.")
+else:
+    print("The strings are not equal.")
+
+# Inequality check
+str1 = "apple"
+str2 = "orange"
+
+if str1 != str2:
+    print("The strings are not equal.")
+else:
+    print("The strings are equal.")
+
+# Lexicographic order check
+str1 = "apple"
+str2 = "banana"
+
+if str1 < str2:
+    print(f"{str1} comes before {str2} in lexicographic order.")
+else:
+    print(f"{str1} comes after {str2} in lexicographic order.")
+
